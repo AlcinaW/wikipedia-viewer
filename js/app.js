ae1f353fe537857bb149bfcd97c1b721430d904e
AlcinaW <white_kaitou@hotmail.com>
2016-06-07 00:05:22 -0400

    comments

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// use the api and search wikipedia in a search
+//bonus: as typing show auto-complete suggestions
+//random button that opens new random article
+
+//JavaScript and maybe JQuery
+
+// random Wikipedia article: https://en.wikipedia.org/wiki/Special:Random
+// https://www.mediawiki.org/wiki/API:Main_page.

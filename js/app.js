1f67a5c99d69b8ece5a2354ea465859f29e70c1a
AlcinaW <white_kaitou@hotmail.com>
2016-06-11 15:25:51 -0400

    Keyup with Enter working
    layout done

--- js/app.js
+++ js/app.js
@@ -8,11 +8,15 @@
 // https://www.mediawiki.org/wiki/API:Main_page.
 
 $(document).ready(function(){
+
+  $("#searchTerm").keyup(function(event){
+      if(event.keyCode == 13){
+          $("#search").click();
+      }
+  });
   $("#search").click(function(){
     var searchTerm = $("#searchTerm").val();
       var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchTerm + "&callback=?";
-        //https://en.wikipedia.org/w/api.php?action=query&srsearch=
-        //action=query&list=search&format=json&srsearch=
       $.ajax({
         type: "GET",
         url: url,
@@ -22,7 +26,7 @@ $(document).ready(function(){
           // console.log(url);
           $("#output").html("");
           for(var i=0; i<data[1].length; i++) {
-            $("#output").prepend("<div class='card'><div class='card-content'><ul><li><a href= " + data[3][i] + ">" + "<span class='card-title'>" + data[1][i] + "</span><i class='small material-icons'>open_in_new</i>" + "</a><p>" + data[2][i] + "</p></li></ul></div></div>");
+            $("#output").prepend("<div class='card'><div class='card-content'><ul><li><a href= " + data[3][i] + ">" + "<span class='card-title'>" + data[1][i] + "</span><i class='tiny material-icons'>open_in_new</i>" + "</a><p>" + data[2][i] + "</p></li></ul></div></div>");
           }
         },
         error: function(errorMessage){
